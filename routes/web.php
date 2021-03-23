70d50a80452bf826f98cdece0e675421ddfdc220
NamioL <gamdelidze@gmail.com>
2021-03-23 11:00:54 +0400

    exersize 2

--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,6 @@ use App\Http\Controllers\Products;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/news', function() {
-    return view('news');
-});
-
-Route::get('/news/{id}', function($id) {
-    return view('news-id', [ 'id' => $id]);
-});
-Route::get('/other', function() {
-    return redirect('/news');
-});
-Route::get('/404', function() {
-    return abort(404);
-});
-
-
 Route::prefix('/products')->group(function(){
     Route::get('/', [Products::class, 'index'])->name('test');
     Route::get('/{id}', [Products::class, 'show']);
